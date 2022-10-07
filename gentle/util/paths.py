13c666beffe9fcf989625d144b9649b3a8f150ed
lcolok <425311101@qq.com>
2022-10-07 16:50:13 +0800

    修改paths.py中的路径，使ffmpeg、ext的m3、k3和exp放在根目录能够被读取

--- gentle/util/paths.py
+++ gentle/util/paths.py
@@ -23,10 +23,9 @@ class SourceResolver:
 
     def get_datadir(self, name):
         return self.get_resource(name)
-
 class PyinstallResolver:
     def __init__(self):
-        self.root = os.path.abspath(os.path.join(getattr(sys, '_MEIPASS', ''), os.pardir, 'Resources'))
+        self.root = os.path.dirname(os.path.realpath(sys.executable))
 
     def get_binary(self, name):
         return os.path.join(self.root, name)
@@ -39,7 +38,7 @@ class PyinstallResolver:
             return get_datadir(name) # DMG may be read-only; fall-back to datadir (ie. so language models can be added)
 
     def get_datadir(self, path):
-        return os.path.join(os.environ['HOME'], '.gentle', path)
+        return os.path.join(self.root, path)
 
 RESOLVER = PyinstallResolver() if hasattr(sys, "frozen") else SourceResolver()
 
